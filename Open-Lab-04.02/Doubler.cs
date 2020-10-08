9e1a1639f7fa5c7a863423e10c71d1e91f684dd2
MilosM33 <milosmarejka89@gmail.com>
2020-10-08 14:49:46 +0200

    init

--- Open-Lab-04.02/Doubler.cs
+++ Open-Lab-04.02/Doubler.cs
@@ -1,12 +1,14 @@
 ﻿using System;
-
+using System.Linq;
 namespace Open_Lab_04._02
 {
     public class Doubler
     {
         public string DoubleChar(string original)
         {
-            throw new NotImplementedException();
+            string temp = "";
+            original.ToCharArray().ToList().ForEach(p=>temp+=p+p.ToString());
+            return temp;
         }
     }
 }
